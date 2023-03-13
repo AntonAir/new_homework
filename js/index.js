89a393b8555d8a923e05d8b4a433be2b49bfbc22
AntonAir <antonairdj@gmail.com>
2023-03-13 22:53:21 +0200

    Дз 1 готово

--- js/index.js
+++ js/index.js
@@ -1,2 +1,24 @@
 "use strict";
 
+let numberOfFilms = +prompt("Скільки фільмів ви вже подивились?", "");
+
+console.log(numberOfFilms);
+
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let q = prompt("Який останній фільм ви дивились?", "");
+let x = prompt("Оцініть цей фільм від 1 до 5", "");
+let q1 = prompt("Який останній фільм ви дивились?", "");
+let x1 = prompt("Оцініть цей фільм від 1 до 5", "");
+
+personalMovieDB.movies[q] = x;
+personalMovieDB.movies[q1] = x1;
+
+console.log(personalMovieDB);
